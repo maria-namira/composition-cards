2523f6369da76c31f97514f1af0dda1b3ce34a69
AppVeyr <ci@appveyor.com>
2023-06-08 17:23:28 +0300

    work

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+export const card1 = {
+  src: 'https://funart.pro/uploads/posts/2021-04/1617392795_41-p-oboi-domik-u-reki-42.jpg',
+  title: 'Заголовок первой карточки',
+  text: 'Очень важный текст для первой карточки'
+}
+
+export const card2 = {
+  title: 'Заголовок второй карточки',
+  text: 'Очень важный текст для второй карточки'
+}
